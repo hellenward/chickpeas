14c973b14b3e42789559c0e670e9bed7304978be
Hellen Ward <hellenwellsward@gmail.com>
2019-05-22 06:31:51 +0100

    Recipe generator now appends recipe onto page

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ $(document).ready(function() {
   $('.btn').click(function() {
     $('.clearMe').empty();
     let chosenRecipe = Math.floor(Math.random() * recipesArray.length);
-    console.log(recipesArray[chosenRecipe]);
-
+    let recipe = (recipesArray[chosenRecipe]);
+    $('.recipeArea').append(`
+        <h2 class="subheading">${recipe.title}</h2>
+        <img src=${recipe.image}> 
+        <p class="blurb">${recipe.blurb}</p>
+        <p class="ingredients">${recipe.ingredients}</p>
+        <p class="method">${recipe.method}</p>
+      `)
 
   })
 
