2a205d64aac807d61e5a515522eb09574531ccd1
Hellen Ward <hellenwellsward@gmail.com>
2019-05-22 07:01:13 +0100

    Created a function to list ingredients in a UL

--- script.js
+++ script.js
@@ -3,18 +3,28 @@ $(document).ready(function() {
   const recipes = window.recipes;
   const recipesArray = recipes.slice();
 
+  function listIngredients(recipe) {
+    let ingredients = recipe.ingredients;
+    for (let i = 0; i < ingredients.length; i++) {
+      $('.ingredientList').append(`
+        <ul>${ingredients[i]}</ul>
+        `)
+    }
+  }
+
   $('.btn').click(function() {
     $('.clearMe').empty();
     let chosenRecipe = Math.floor(Math.random() * recipesArray.length);
     let recipe = (recipesArray[chosenRecipe]);
     $('.recipeArea').append(`
         <h2 class="subheading">${recipe.title}</h2>
-        <img src=${recipe.image}> 
+        <img src=${recipe.image}>
         <p class="blurb">${recipe.blurb}</p>
-        <p class="ingredients">${recipe.ingredients}</p>
+        <h4>Ingredients:</h4>
+        <ul class="ingredientList"></ul>
         <p class="method">${recipe.method}</p>
       `)
-
+      listIngredients(recipe);
   })
 
 
